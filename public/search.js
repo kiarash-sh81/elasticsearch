be5a5c400dbbf19f3a842edd9929e5be80a4322e
kiarash-sh81 <kiarashshahroudi@gmail.com>
2023-04-23 12:05:34 +0330

    search engin ready

--- public/search.js
+++ public/search.js
@@ -1,22 +1,23 @@
 const input = document.getElementById("search");
 const datalist = document.getElementById("blog-result");
-input.addEventListener("keyup", async function() {
-    datalist.innerHTML = "";
+input.addEventListener("keyup" , async function(){
+    datalist.innerHTML = ""
     const search = this.value;
-    const result = await (await fetch(`http://localhost:3333/blog/findBlogByMultifield?search=${search}`)).json()
-    for (const blog of result) {
+    const resualt = await (await fetch(`http://localhost:3333/blog/multiFieldsSearch?search=${search}`)).json()
+    const finalResualt = resualt.data.blog
+    for(const blog of finalResualt){
         const item = document.createElement("li");
-        item.innerHTML = blog._source.title
+        item.innerHTML = blog._source.title;
         const br = document.createElement("br");
+        const br2 = document.createElement("br")
         const small = document.createElement("small");
         const strong = document.createElement("strong");
-        small.innerHTML = blog._source.text
-        strong.innerHTML = blog._source.author
-        item.appendChild(br)
-        item.appendChild(br)
-        item.appendChild(small)
-        item.appendChild(br)
-        item.appendChild(strong)
+        small.innerHTML = blog._source.text;
+        strong.innerHTML = blog._source.author;
+        item.appendChild(br);
+        item.appendChild(small);
+        item.appendChild(br2);
+        item.appendChild(strong);
         datalist.appendChild(item)
     }
 })
